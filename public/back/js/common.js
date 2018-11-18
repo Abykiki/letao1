132f87b6ad7028c77dc15084e18ef6e9b0fc0b1b
Abykiki <1981161363@qq.com>
2018-11-18 11:22:42 +0800

    登录页完成 提交

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,10 +1,12 @@
 // 1.进度条功能
 // 在第一个ajax请请求时,开启进度条,在所有的ajax请求结束后,关闭进度条
-$(document).ajaxStart(function(){
+
+$(document).ajaxStart(function(){ 
     // 开启进度条
     NProgress.start();
 });
 // 关闭进度条
+// 加延迟是为了测试使用的,平时写不需要加
 $(document).ajaxStop(function(){
     setTimeout(function(){
         NProgress.done();
@@ -28,6 +30,10 @@ $(function(){
     // 3.退出功能
     $(".lt_topbar .icon_right").click(function(){
         // 点击按钮,显示模态框
+        $('#logoutModal').modal("show");
+
+    });
+    //模态框的按钮点击事件
         $("#logoutBtn").click(function(){
             // 发送ajax请求,让后台销毁当前用户的登录状态
             $.ajax({
@@ -39,7 +45,6 @@ $(function(){
                         location.href = "login.html";
                     }
                 }
-            })
         })
     })
 })
